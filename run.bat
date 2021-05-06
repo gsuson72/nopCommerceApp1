5090485345e65cedeb0b82e744019ca76079e3b0
gsuson72 <georgesuson@gmail.com>
2021-05-06 23:10:13 +0800

    my fixed test

--- run.bat
+++ run.bat
@@ -1,1 +1,6 @@
-pytest -v --capture=tee-sys --html=Reports\report.html testCases/test_login.py --browser chrome
+rem pytest -v -s testCases\test_login.py --browser chrome
+rem pytest -v -s testCases\test_login.py --browser firefox
+rem pytest -v -s  -n=2 testCases/test_login.py --browser firefox
+pytest -v --capture=tee-sys -n=2 --html=Reports\report.html testCases\test_login.py --browser chrome
+rem pytest -v --capture=tee-sys --html=Reports\report.html testCases\test_addCustomer.py --browser chrome
+rem pytest -v --capture=tee-sys -m "sanity" --html=Reports\report.html testCases\ --browser chrome
